5267b3a095a4e6ed6b874f4096c3035c73e77293
Artiom_Sokolov <sokolova-net@mail.ru>
2023-08-09 14:19:59 +0300

    mobile menu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,15 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+// Header
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  if (targetElement.closest('.search-header__icon')) {
+    document.documentElement.classList.add('search-open');
+    e.preventDefault();
+  } else if (!targetElement.closest('.search-header')) {
+    document.documentElement.classList.remove('search-open');
+  }
+});
